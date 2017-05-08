39ce9e18f1dce1f1c1d48b5a2d387195574e9c38
qc510 <qc510@nyu.edu>
2017-05-07 21:03:41 -0400

    Update product_spark.py

--- product_spark.py
+++ product_spark.py
@@ -3,7 +3,7 @@ from operator import mul
 
 #This product_spark is to find the product of number from 1 to 1,000
 if __name__ == '__main__':
-sc = SparkContext("local", "product_spark")
+    sc = SparkContext("local", "product_spark")
     # Create an RDD of numbers range from 1 to 1000
     nums = sc.parallelize(range(1,1001))
     # fold method to calculate.
